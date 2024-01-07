a5276688e15b752db7f387db7d096c6e87995305
Семён Каунов <kaunov.semyon@gmail.com>
2024-01-07 18:13:04 +0300

    app added by PoetFromIce

--- sentiment.py
+++ sentiment.py
@@ -1,4 +1,13 @@
+import io
+import streamlit as st
 from transformers import pipeline
+pipe = pipeline("translation", model="Helsinki-NLP/opus-mt-en-ru")
 
-en_fr_translator = pipeline("Helsinki-NLP/opus-mt-en-ru")
-en_fr_translator("How old are you?")
+st.title('Translation App')
+# поле для ввода текста
+text_input = st.text_area("Введите текст для перевода:", value="", height=200)
+if text_input:
+   result = pipe(text_input)[0]['translation_text'] # перевод текста
+   st.write(result) # вывод перевода
+else: 
+   st.warning("Please enter text to translate.")
